311c6d05c1787620401e895c73c07556d754d6cc
LEDidems <adriendem@gmail.com>
2025-11-28 09:04:24 +0100

    They deleted my work wtf

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,1 +1,12 @@
-aaaaa
+def ask_text(text):
+    a = 0
+    while a == 0 or a == None or a == '':
+        a = input(text)
+    return a.strip()
+def ask_choice(message, option):
+    a = 0
+    while a == 0:
+        for i in range(len(options):
+            a = input(message)
+    return options[int(a)-1]
+f = ask_choice("Select an option:", ["Option 1", "Option 2", "Option 3"])
