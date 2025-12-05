69d959e10f071b3b01b9933390f8f2536a649a28
Pikono95 <Nodaubercies@gmail.com>
2025-12-05 08:31:30 +0100

    update input utils

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,8 +1,11 @@
+import json
+
 def ask_text(text):
     a = 0
     while a == 0 or a == None or a == '':
         a = input(text)
     return a.strip()
+
 def ask_choice(message, option):
     a = 0
     while a == 0:
@@ -11,3 +14,16 @@ def ask_choice(message, option):
             print(str(i+1)+". "+option[i])
         a = int(input()) 
     return option[a-1]
+
+def ask_number(rep,maxi = None,mini = None):
+    numb = int(input(rep))
+    if maxi = None and mini = None:
+        while numb < mimi or numb > maxi:
+            print("Please enter a number between ",mini," and ",maxi,".")
+            numb = int(input(rep))
+    return numb
+
+def load_file(file_path):
+    with open(file_path, 'r') as file:
+        rep = json.load(file)
+    return rep
--- utils/input_utils2.py
+++ utils/input_utils2.py
@@ -1,12 +1,1 @@
-import json
-def ask_number(rep,maxi = None,mini = None):
-    numb = int(input(rep))
-    if maxi = None and mini = None:
-        while numb < mimi or numb > maxi:
-            print("Please enter a number between ",mini," and ",maxi,".")
-            numb = int(input(rep))
-    return numb
-def load_file(file_path):
-    with open(file_path, 'r') as file:
-        rep = json.load(file)
-    return rep
+
